4d8be7e964f552aa365b8533f3f9f6f753d6f429
Ashcoder666 <ashirnbasheer@gmail.com>
2023-10-22 12:20:28 +0530

    added get route for contacts using mux and http

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/Ashcoder666/go-contact-api
 
 go 1.21.1
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,11 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 	// "net/http"
 )
 
+//	type contact struct{
+//		Name string `json:"name"`
+//		Number int `json:"number"`
+//	}
+var db = make(map[string]int)
+
 func main() {
-	fmt.Println("Hello world")
-	// http.ListenAndServe(":4000",)
+	fmt.Println("server is starting on port 4000")
+	db["ashir"] = 8157987955
+	r := mux.NewRouter()
+	r.HandleFunc("/contact", getContactHandler).Methods("Get")
+	r.HandleFunc("/contact", postContactHandler).Methods("Post")
+	err := http.ListenAndServe(":4000", r)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// show all contacts in map
+func getContactHandler(w http.ResponseWriter, r *http.Request) {
+	var response = map[string]interface{}{
+		"message": "success",
+		"data":    db,
+	}
+	dbjson, err := json.MarshalIndent(response, "", "\t")
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(dbjson)
+}
+
+// add contacts into map
+func postContactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("helloooo"))
 }
